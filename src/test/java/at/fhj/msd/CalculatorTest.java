fa6d76d1e19735b1c3668ae635f3f0f988fd3c23
Headline42 <jonas.sieff@gmx.at>
2024-06-04 14:49:31 +0200

    minor changes

--- src/test/java/at/fhj/msd/CalculatorTest.java
+++ src/test/java/at/fhj/msd/CalculatorTest.java
@@ -1,2 +1,35 @@
-package at.fhj.msd;public class CalculatorTest {
-}
+package at.fhj.msd;
+
+public class CalculatorTest {
+
+        public  double add(double number1, double number2) {
+
+            return number1 + number2;
+        }
+
+        public  double minus(double number1, double number2) {
+            return number1 - number2;
+
+        }
+        public double divide(double number1, double number2) {
+
+            if(number2 != 0) {
+                return number1 / number2;
+            }else {
+
+                return -1;
+            }
+        }
+
+
+        public double multiply(double number1, double number2){
+
+
+            return number1 * number2;
+        }
+
+
+    }
+
+
+
