588c38931765f5383a86843dea7282fdf819034c
Headline42 <jonas.sieff@gmx.at>
2024-06-18 21:04:47 +0200

    added new log4j2 file

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,29 +1,35 @@
 package at.fhj.msd;
 
-public class Calculator {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class Calculator {
+    private static Logger logger = LogManager.getLogger();
     public double add(double number1, double number2) {
+        logger.debug("Called with: " + number1 + " and " + number2);
 
         return number1 + number2;
     }
 
     public double minus(double number1, double number2) {
+        logger.debug("Called with: " + number1 + " and " + number2);
         return number1 - number2;
 
     }
 
     public double divide(double number1, double number2) {
-
+        logger.debug("Called with: " + number1 + " and " + number2);
         if (number2 != 0) {
             return number1 / number2;
         } else {
-
+            logger.error("Called with: " + number1 + " and " + number2);
             return -1;
         }
     }
 
 
     public double multiply(double number1, double number2) {
+        logger.debug("Called with: " + number1 + " and " + number2);
 
 
         return number1 * number2;
@@ -33,6 +39,7 @@ public class Calculator {
 
 
     public int factorial(int n) {
+        logger.debug("Called with:" + n );
         if (n < 0) {
             return -1;
         }
