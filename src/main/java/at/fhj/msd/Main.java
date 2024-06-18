c7a3735fd44c02f1eabbfe9b6f6fc87f8f15afea
Headline42 <jonas.sieff@gmx.at>
2024-06-18 20:58:19 +0200

    minor changes

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,12 +1,12 @@
 package at.fhj.msd;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class Main {
+    // Creating a logger
+    private static Logger logger = LogManager.getLogger();
 
-
-
-
+    // Log messages
     public static void main(String[] args) {
-
 
         Calculator calc = new Calculator();
 
@@ -15,6 +15,10 @@ public class Main {
        calc.divide(1,1);
        calc.multiply(1,1);
 
+
+        logger.error("It is an error logger.");
+
+        logger.info("It is a info logger.");
 
         System.out.println("Sieff Jonas");
     }
